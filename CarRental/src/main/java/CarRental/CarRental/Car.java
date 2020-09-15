9531c76c1bde6faefc39631b039c433d98520fc7
Rektzie <43027690+Rektzie@users.noreply.github.com>
2020-09-15 17:47:53 +0700

    Add files via upload

---
+++ CarRental/src/main/java/CarRental/CarRental/Car.java
@@ -0,0 +1,41 @@
+package CarRental.CarRental;
+
+public class Car {
+	private String color;
+	private String type;
+	private Integer displacement;
+	private Boolean sunroof;
+	private Integer speed;
+	public String getColor() {
+		return color;
+	}
+	public String getType() {
+		return type;
+	}
+	public Integer getDisplacement() {
+		return displacement;
+	}
+	public Boolean getSunroof() {
+		return sunroof;
+	}
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void setDisplacement(Integer displacement) {
+		this.displacement = displacement;
+	}
+	public void sunroof(Boolean sunroof) {
+		this.sunroof = sunroof;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+}
+
+
